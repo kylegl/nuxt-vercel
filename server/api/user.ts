95e08bd5cf31ff2f97d79a3e1e54ff4bd545f931
Kyle <lauber.kyle@gmail.com>
2023-01-14 21:58:54 -0800

    chore: testing prisma

---
+++ server/api/user.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+  const clientInput = { username: await readBody(event) }
+  return await prisma.user.create({ data: clientInput })
+})
